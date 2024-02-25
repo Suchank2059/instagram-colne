55fc584ead7c0ed994c74ac8d69bbfa4c3f84d91
Suchank2059 <132805306+Suchank2059@users.noreply.github.com>
2024-02-25 20:47:29 +0545

    INstall & configure alpine intersect plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
-import './bootstrap';
+import "./bootstrap";
 import { livewire_hot_reload } from "virtual:livewire-hot-reload";
 livewire_hot_reload();
 
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
 
+import intersect from "@alpinejs/intersect";
+
+Alpine.plugin(intersect);
 Alpine.start();
