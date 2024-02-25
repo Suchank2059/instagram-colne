f2b0396f76d99a40c90d89af0a459d40427e5509
Suchank2059 <132805306+Suchank2059@users.noreply.github.com>
2024-02-25 20:52:53 +0545

    Install & configure swiper js

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,3 +10,17 @@ import intersect from "@alpinejs/intersect";
 
 Alpine.plugin(intersect);
 Alpine.start();
+
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+});
