084e88027b2156a3dc76180c5371fb1f1d76c1ea
Suchank2059 <132805306+Suchank2059@users.noreply.github.com>
2024-02-28 19:28:58 +0545

    Seed dummy Post data into database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,15 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Post::factory(20)->create(['type' => 'reel']);
+        Post::factory(rand(10, 40))->create(['type' => 'post']);
     }
 }
